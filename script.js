2f7e6372253fbb2f756646d1c21e5b45e1ee0f66
mfonido mark <favourmark05@gmail.com>
2020-08-30 12:02:56 +0100

    first

---
+++ script.js
@@ -0,0 +1,24 @@
+  function loadClient() {
+    gapi.client.setApiKey(" AIzaSyBKVbMzDox2ybB35auAv_wp5sB5Lmar7xE ");
+    return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/customsearch/v1/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded before calling this method.
+  function execute() {
+    return gapi.client.search.cse.list({
+      "imgColorType": "mono",
+      "imgDominantColor": "teal",
+      "imgSize": "MEDIUM",
+      "imgType": "lineart",
+      "safe": "active",
+      "searchType": "image",
+      "siteSearchFilter": "siteSearchFilterUndefined"
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  gapi.load("client");
